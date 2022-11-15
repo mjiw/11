6c5d2cbdfc46961b258a492099c790c5e7d6adba
mjiw <113112060+mjiw@users.noreply.github.com>
2022-11-15 16:40:35 +0900

    test3

--- main.c
+++ main.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void swap(int *x,int *y){
+	//int *x=&a;
+	//int *y=&b;
+	
+	//변수의 값을 변경 
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+	
+}
 
 int main(int argc, char *argv[]) {
-	int i=300;
+	int a=3;
+	int b=5;
 	
-	int *pi=&i;
-	char *pc=&i;
-	
-	printf("%i, %i, %i\n",i,*pi,*pc); 
+	swap(&a,&b);
+	printf("a:%i, b:%i\n",a,b);
 	return 0;
+	
 }
