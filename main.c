79d477aaeca53166b7cce17232a817c33b4dc6e5
mjiw <113112060+mjiw@users.noreply.github.com>
2022-11-15 16:52:46 +0900

    test4

--- main.c
+++ main.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void swap(int *x,int *y){
-	//int *x=&a;
-	//int *y=&b;
-	
-	//변수의 값을 변경 
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-	
-}
 
 int main(int argc, char *argv[]) {
-	int a=3;
-	int b=5;
+	int i;
+	int grade[5];
 	
-	swap(&a,&b);
-	printf("a:%i, b:%i\n",a,b);
+	for(i=0;i<5;i++){
+		printf("grade[%i]=",i);
+		scanf("%d",&grade[i]);
+	} 
+	
+	//int *number;
+	int sum;
+	for(i=0; i<5; i++){
+		//number=&grade[i];
+		//printf("grade[%i]=%i\n",i,*number);
+		
+		printf("grade[%i]=%i\n",i,*(grade+1));
+		sum+=*(grade+i);
+	}
+	printf("average:%i",sum/5);
 	return 0;
-	
 }
